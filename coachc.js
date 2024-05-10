9044b690b395246454ca5217f5cd221317f67eaf
im-tchia <167340312+im-tchia@users.noreply.github.com>
2024-05-10 09:20:24 +0000

    alpha version with basic interface to hugging face API done

--- coachc.js
+++ coachc.js
@@ -2,36 +2,39 @@ var ask = "";
 var answer = "";
 var buffer = "";
 
-//alert("hi my name is codi!");
+sendAns("Hi my name is Codi, how can I help you?");
 
-//ask = prompt("what wld you like to ask CODI?");
-//sendAsk(ask);
+// test code below which worked-----
+//queryG({"inputs": "Can you please let us know more details about Singapore"}).then((response) => {
+//	console.log(JSON.stringify(response));
+//});
+//-------------------
 
-//queryH({inputs:{question:"What is my name?",context:"My name is Clara and I live in Berkeley."}}).then((response) => {
-//  console.log(JSON.stringify(response))});
-
-
-//answer = queryH({inputs:{question:ask,context:""}}).then((response) => {
-//  console.log(JSON.stringify(response))});
-
-queryH({inputs:{question:"What is my name?",context:"My name is Clara and I live in Berkeley."}}).then((response) => {
-  console.log(JSON.stringify(response));
-});
-//answer = queryH({"who are you?",""});
-
-document.getElementsByClassName("msg-page")[document.getElementsByClassName("msg-page").length-1].insertAdjacentHTML("beforeend",createOutChat(answer));
-
-//document.getElementById("lastMsg").insertAdjacentHTML("afterend",createOutChat(prompt("say something!")))
-
-//// document.getElementById("lastMsg").appendChild(createOutChat(prompt("say something!")))
 
 function sendAsk(text = '') {
+  
   document.getElementsByClassName("msg-page")[document.getElementsByClassName("msg-page").length-1].insertAdjacentHTML("beforeend",createInChat(text));
-var textr = "noted."
-  document.getElementsByClassName("msg-page")[document.getElementsByClassName("msg-page").length-1].insertAdjacentHTML("beforeend",createOutChat(textr));
+
+  console.log("calling sendAns using <"+ text+ ">");
+  
+  queryG({"inputs" : text}).then((response) => 
+    {
+    console.log(JSON.stringify(response))
+    sendAns(response[0].generated_text)
+    }
+  )
+
+  // sendAns(answer);
+  //var textr = "noted."
+//  document.getElementsByClassName("msg-page")[document.getElementsByClassName("msg-page").length-1].insertAdjacentHTML("beforeend",createOutChat(textr));
 
 
 }
+function sendAns(text = ''){
+  document.getElementsByClassName("msg-page")[document.getElementsByClassName("msg-page").length-1].insertAdjacentHTML("beforeend",createOutChat(text));
+
+}
+
 
 function createInChat(text = '') {
   return`
@@ -72,6 +75,25 @@ function createOutChat(text = '') {
   };
 
 
+  async function queryG(data) {
+    console.log("queryG called");
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct",
+      {
+        headers: { 
+          Authorization: "Bearer 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",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await response.json();
+    
+    console.log("quesryG result: " + result);
+    return result;
+  }
+ 
   async function queryH(data) {
     const response = await fetch(
       "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct",
@@ -89,3 +111,17 @@ function createOutChat(text = '') {
 
    // query({"inputs": "Can you please let us know more details about your "}).then((response) => {console.log(JSON.stringify(response));
   }
+  async function queryP(data) {
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/microsoft/Phi-3-mini-128k-instruct",
+      {
+        headers: { Authorization: "" },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await response.json();
+    return result;
+  }
+  
+  
